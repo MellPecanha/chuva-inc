3e7ef3a8f9d17173f2b1c36e4b35bf70f29174cb
Mell Peçanha <mellpecanha@gmail.com>
2024-05-10 18:04:45 -0300

    feat: connection components on app module

--- ts/src/app/app.module.ts
+++ ts/src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import { ContentComponent } from './components/content/content.component';
+import { AsideComponent } from './components/aside/aside.component';
+import { HeaderComponent } from "./components/header/header.component";
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    ContentComponent,
+    AsideComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [ContentComponent, AsideComponent, HeaderComponent, FooterComponent]
 })
-export class AppModule { }
+export class AppModule {}
